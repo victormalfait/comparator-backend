657183258c71293fa2e7ac49674f14a0ade1013d
VIctor Malfait <victor.malfait@adopteunmec.com>
2020-04-15 14:48:58 +0200

    Save lat and lng store

--- src/models/store.model.ts
+++ src/models/store.model.ts
@@ -1,44 +1,56 @@
-import {Entity, model, property} from '@loopback/repository';
+import { Entity, model, property } from "@loopback/repository";
 
 @model()
 export class Store extends Entity {
   @property({
-    type: 'string',
+    type: "string",
     id: true,
     generated: false,
-    required: false,
+    required: false
   })
   id: number;
 
   @property({
-    type: 'string',
-    required: true,
+    type: "string",
+    required: true
   })
   name: string;
 
   @property({
-    type: 'number',
-    required: true,
+    type: "number",
+    required: true
   })
   company: number;
 
   @property({
-    type: 'string',
-    required: true,
+    type: "string",
+    required: true
   })
   address: string;
 
   @property({
-    type: 'string',
-    required: true,
+    type: "string",
+    required: true
   })
   city: string;
 
   @property({
-    type: 'number',
-    required: true,
+    type: "number",
+    required: true
   })
   zipcode: number;
+
+  @property({
+    type: "number",
+    required: true
+  })
+  lat: number;
+
+  @property({
+    type: "number",
+    required: true
+  })
+  lng: number;
 
   constructor(data?: Partial<Store>) {
     super(data);
